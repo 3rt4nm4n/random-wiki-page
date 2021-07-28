f7689e0e4d3299d0bde2eb6045cc6fd2aac033e6
3rt4nm4n <nigbuk1998@gmail.com>
2021-07-28 12:14:28 +0300

    Initial Commit

---
+++ RandomWikiPageGen.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup
+import webbrowser
+import requests
+
+language1=""
+language1=input("Which language would you like to read in? EN, FR, DE, RU, IT, TR, RO, PL, ES: ").lower()
+
+while(True):
+    
+    speciallink="https://"+language1+".wikipedia.org/wiki/Special:Random"
+    wikiURL=requests.get(speciallink)
+    soup = BeautifulSoup(wikiURL.content, "html.parser") 
+    title=soup.find(class_="firstHeading").text  
+    
+    print(f"{title} \nDo you want to view it? (Y/N)")
+    ans=input("").lower()    
+    
+    if ans=="y":
+        urll="https:http://"+language1+".wikipedia.org/wiki/%s"%title
+        print (urll)
+        wikiURL=urll
+        webbrowser.open(wikiURL)
+        break
+    elif ans=="n":
+        print("Try again!")
+        continue
+    else:
+        print("Wrong choice!")
+        break
+    
